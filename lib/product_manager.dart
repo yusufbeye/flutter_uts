5800f096082c83c15ef8692d70dde3c65e13dac3
yusuf beye <ubeye46@gmail.com>
2019-05-11 18:45:02 +0700

    test

---
+++ lib/product_manager.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import './products.dart';
+class ProductManager extends StatefulWidget {
+  @override
+  _ProductManagerState createState() => _ProductManagerState();
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+      margin: EdgeInsets.all(10.0),
+      child: RaisedButton(
+        child: Text('Add Product'),
+        onPressed: () {
+          setState(() {
+            _products.add('Food Vaganza');
+            print(_products);
+          });
+        },
+      ),
+    ),
+    Products(_products),
+      ],
+    );
+  }
+}
+
---
+++ lib/products.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  List<String> products;
+  Products(this.products);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: products
+          .map(
+            (el) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text(el),
+                    ],
+                  ),
+                ),
+          )
+          .toList(),
+    );
+  }
+}
